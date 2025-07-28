310a8f5b4fb9545c10f6ce32ed3e9f78059eb4cb
Jayvee Maniquiz <136257803+JayveeFortissimo@users.noreply.github.com>
2025-07-29 00:07:39 +0800

    feat: add Experience

--- src/utils/experience.ts
+++ src/utils/experience.ts
@@ -10,6 +10,21 @@ const MyExperience: Experience[] = [
     company: "GEER IT SOLUTIONS (OJT)",
     year: 2025,
   },
+  {
+    title: "Cristobal Reservation",
+    company: "Capstone Project",
+    year: 2024,
+  },
+  {
+    title: "Nombra System",
+    company: "Personal Project",
+    year: 2024,
+  },
+  {
+    title: "Website Portfolio",
+    company: "My Portfolio",
+    year: 2024,
+  },
 ];
 
 export default MyExperience;
