b95873b9d1f6a68a58f45ba323c508cc4cfd1d98
Jayvee Maniquiz <136257803+JayveeFortissimo@users.noreply.github.com>
2025-07-28 15:54:45 +0800

    feat: add font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    
+    <header>
+      <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap" rel="stylesheet"/>
+
+    </header>
       <body
       className="inter"
       >
