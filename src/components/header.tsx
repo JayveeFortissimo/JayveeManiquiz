4b4d37e90870ca9226a7a1752a6e2a6d4dc69e60
Jayvee Maniquiz <136257803+JayveeFortissimo@users.noreply.github.com>
2025-07-29 01:02:14 +0800

    feat: Add Link redirects social media

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,12 @@
 "use client";
-import { MapPin, Facebook, Github } from "lucide-react";
+import { MapPin, Facebook, Github, Mail } from "lucide-react";
 import { Button } from "./ui/button";
 import { DayNightSwitch } from "./day-night";
 import { useTheme } from "next-themes";
 import { useState } from "react";
+import { SparklesText } from "@/components/magicui/sparkles-text";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 export default function Header() {
   const [isClicked, setCliecked] = useState<boolean>(false);
 
@@ -18,6 +21,7 @@ export default function Header() {
   } else {
     setTheme("light");
   }
+  const redirect = useRouter();
 
   return (
     <div className="min-h-[10rem]  flex items-center flex-col px-2">
@@ -30,9 +34,9 @@ export default function Header() {
         />
         <div className="w-full">
           <header className="w-full flex justify-between items-center pr-3">
-            <h1 className="text-[1.1rem] md:text-2xl font-bold">
+            <SparklesText className="text-[1.1rem] md:text-2xl font-bold">
               Jayvee Maniquiz
-            </h1>
+            </SparklesText>
             <DayNightSwitch className="h-[1.8rem] " onClick={toggleTheme} />
           </header>
           <div className="flex items-center gap-1 mt-[0.1rem]">
@@ -45,43 +49,29 @@ export default function Header() {
             Full-Stack Web Developer
           </h1>
           <footer className="mt-2 flex items-center flex-wrap gap-3 w-full pr-3">
-            <Button className="h-[1.8rem] bg-blue-500 cursor-pointer">
+            <Button  className="h-[1.8rem] bg-blue-500 cursor-pointer" onClick={() => redirect.push("https://www.facebook.com/jayvee.maniquiz.779/")}>
+            
               <Facebook />
-              Facebook
+             <p> Facebook</p>
             </Button>
-            <Button className="h-[1.8rem]  cursor-pointer">
+            <Button className="h-[1.8rem]  cursor-pointer" onClick={() => redirect.push("https://github.com/JayveeFortissimo?tab=repositories")}>
               <Github />
               Github
             </Button>
 
-            <Button
-              asChild
-              className="h-[1.8rem]  flex-auto bg-yellow-500 text-white    cursor-pointer hidden md:flex"
-            >
-              <a
-                href="/docs/Jayvee-Maniquiz-CV.pdf"
-                download
-                className="bg-primary text-black px-4 py-2 rounded"
-              >
-                Download Resume
-              </a>
+            <Button className="h-[1.8rem]  flex-auto     cursor-pointer hidden md:flex" 
+            onClick={()=> redirect.push('https://mail.google.com/mail/?view=cm&fs=1&to=maniquizjayvee5@gmail.com')}>
+              <Mail />
+              Send Email
             </Button>
           </footer>
         </div>
       </header>
 
       <div className="w-full container mx-auto mt-5 flex items-center justify-center md:hidden ">
-        <Button
-          asChild
-          className="h-[1.8rem]  flex-auto bg-yellow-500 text-white    cursor-pointer"
-        >
-          <a
-            href="/docs/Jayvee-Maniquiz-CV.pdf"
-            download
-            className="bg-primary text-black px-4 py-2 rounded"
-          >
-            Download Resume
-          </a>
+        <Button className="h-[1.8rem]  flex-auto     cursor-pointer">
+          <Mail />
+          Send Email
         </Button>
       </div>
     </div>
